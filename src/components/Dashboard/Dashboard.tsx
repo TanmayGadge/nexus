7e00d8d359bd55d1b4e3368100aefb5ab7b07393
Tanmay Gadge <tanmaygadge9273@gmail.com>
2025-09-17 14:04:45 +0530

    build: configure recharts

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,53 @@
+import { Car } from "lucide-react";
+import React from "react";
+
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+
+interface data {
+  name: string;
+  uv: number;
+  pv: number;
+  amt: number;
+}
+
+const Dashboard = () => {
+  // const data = [
+  //   { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+  //   { name: "Page B", uv: 350, pv: 2400, amt: 2400 },
+  //   { name: "Page A", uv: 200, pv: 2400, amt: 2400 },
+  //   { name: "Page A", uv: 425, pv: 2400, amt: 2400 },
+  //   { name: "Page A", uv: 450, pv: 2400, amt: 2400 },
+  //   { name: "Page A", uv: 300, pv: 2400, amt: 2400 },
+  //   { name: "Page A", uv: 400, pv: 2400, amt: 2400 }
+  // ];
+  const [data, setData] = React.useState<data[]>([
+    { name: "Page 1", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page 2", uv: 350, pv: 2400, amt: 2400 },
+    { name: "Page 3", uv: 200, pv: 2400, amt: 2400 },
+    { name: "Page 4", uv: 425, pv: 2400, amt: 2400 },
+    { name: "Page 5", uv: 450, pv: 2400, amt: 2400 },
+    { name: "Page 6", uv: 300, pv: 2400, amt: 2400 },
+    { name: "Page 7", uv: 400, pv: 2400, amt: 2400 },
+  ]);
+
+  setInterval(() => {
+    let uvValue: number = Math.floor(Math.random() * 600) + 100;
+    let count = data.length + 1;
+    setData([...data, { name: `Page ${count}`, uv: uvValue, pv: 2400, amt: 2400 }]);
+    count ++;
+  }, 3000);
+
+  return (
+    <div className="bg-red-500">
+      <h1 className="font-bold">Dashboard</h1>
+      <LineChart width={600} height={300} data={data}>
+        {/* <CartesianGrid /> */}
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Line dataKey={"uv"} type={"monotone"} stroke="purple" strokeWidth={2} />
+      </LineChart>
+    </div>
+  );
+};
+
+export default Dashboard;
